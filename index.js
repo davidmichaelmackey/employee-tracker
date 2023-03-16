37d11e1488ccdb07d320d04e7b22fceb0cf34e7c
David Mackey <davidmackey92@gmail.com>
2023-03-16 02:45:34 -0400

    index.js - mysql connection

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "reno911",
+    database: "employee_tracker_db",
+  },
+  dots(),
+  wingT(),
+  connectedTo(),
+  console.log("|| Connected to employee_tracker_db ||"),
+  connectedTo(),
+  wingB(),
+  dots(),
+);
+
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  mainMenu();
+});
