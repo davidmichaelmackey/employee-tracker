c99df0d5daefe07a15ca25ff1db73680a382fed3
David Mackey <davidmackey92@gmail.com>
2023-03-16 02:47:45 -0400

    index.js - main menu, menu prompts, function calls

--- index.js
+++ index.js
@@ -18,7 +18,56 @@ const connection = mysql.createConnection(
   dots(),
 );
 
-
+const mainMenu = () => {
+  inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: '\n=================================\n|| What would you like to do ? ||\n=================================\n.\n.\n.',
+    choices: [
+      "View Departments",
+      "View Roles",
+      "View Employees",
+      "Add Department",
+      "Add Role",
+      "Add Employee",
+      "Update Employee Role",
+      "Exit"
+    ]
+  })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Employees":
+          viewEmployees();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Update Employee Role":
+          updateEmployeesRole();
+          break;
+        case "Exit":
+          exitApp();
+          break;
+        default:
+          console.log("Invalid option, please try again...");
+          mainMenu();
+          break;
+      }
+    })
+    .catch(err => console.log(err));
+};
 
 connection.connect((err) => {
   if (err) throw err;
